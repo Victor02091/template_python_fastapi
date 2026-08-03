c1d108ba0c84968a1dc32fb5bd7980f0e0d7caab
Victor Osso <victor.osso@mpdata.fr>
2026-08-03 22:12:18 +0200

    fix: make main.py templatable

--- template/app/core/middleware.py
+++ template/app/core/middleware.py
@@ -6,6 +6,8 @@ def setup_middlewares(app: FastAPI) -> None:
     """Register application middlewares."""
     app.add_middleware(
         CORSMiddleware,
+        # Allow all origins for CORS. In production,
+        # you may want to restrict this to specific domains.
         allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
